8ab67d236acdd2e3b1ba1ed7916bd0b5bd42a5cf
Hrithikcse52 <hrithik.cse52@gmail.com>
2021-09-24 00:09:08 +0530

    CSS and JS all working Now

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Editor from "./Editor";
 import "./app.css";
 import Nav from "./Nav";
 const App = () => {
-  const srcDoc = `<html>
-    <style>
-    .hel{
-        border: 1px solid black;
-    }
-    </style>
-    <script></script>
-    <body>
-    <div class="hel">
-    Hello</div> 
-    </body>
-    </html>`;
+  const defHTML = `<form method="GET">
+  What is your name: <input type="text" size="20" id="myname" onkeydown = "if (event.keyCode == 13)  document.getElementById('mybutton').click()"   />
+<input type="text" style="display: none;" />
+<button type="button" id="mybutton">Submit</button>
+</form>
+<div id="hellomessage"></div>
+`;
+
+  const defCSS = `body {
+  font-size:1em;
+  font-family:Arial;
+  background:#eee;
+}
+
+#hellomessage {
+  font-weight:bold;
+}`;
+
+  const defJS = `function ShowHelloMessage() {
+  var name = document.getElementById("myname");
+  document.getElementById("hellomessage").innerHTML = "Hello, " + name.value;
+}
+document.getElementById("mybutton").onclick = ShowHelloMessage;`;
+
+  const [html, setHtml] = useState(defHTML);
+  const [css, setCss] = useState(defCSS);
+  const [js, setJs] = useState(defJS);
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>
+    `);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
   return (
     <>
       <div className="header">
         <Nav />
         <div className="top-bar">
-          <Editor heading="HTML" />
-          <Editor heading="CSS" />
-          <Editor heading="JS" />
+          <Editor language="xml" title="HTML" value={html} onChange={setHtml} />
+          <Editor language="css" title="CSS" value={css} onChange={setCss} />
+          <Editor
+            language="javascript"
+            title="JS"
+            value={js}
+            onChange={setJs}
+          />
         </div>
         <div className="bottom-bar">
           <iframe
             srcDoc={srcDoc}
             title="Output"
+            sandbox="allow-forms allow-popups allow-scripts allow-same-origin allow-modals"
+            frameBorder="0"
             style={{
               marginTop: "1rem",
               border: "1px solid black",
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { Controlled } from "react-codemirror2";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
 import "./editor.css";
-const Editor = (props) => {
+const Editor = ({ title, language, value, onChange }) => {
+  const handleChange = (editor, data, value) => {
+    onChange(value);
+  };
   return (
     <>
-      <div className="code">
-        <Controlled
-          value="hello"
-          options={{
-            mode: "xml",
-            theme: "material",
-            lineNumbers: true,
-          }}
-        />
+      <div className="container">
+        <div className="title">{title}</div>
+        <div className="code">
+          <Controlled
+            onBeforeChange={handleChange}
+            value={value}
+            options={{
+              lineWrapping: true,
+              mode: language,
+              theme: "material",
+              lineNumbers: true,
+            }}
+          />
+        </div>
       </div>
     </>
   );
